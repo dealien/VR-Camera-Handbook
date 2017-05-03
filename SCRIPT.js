251920cc7e7dd387eb1ce9fa168be5c0013f1dd5
dealien <dragonofawsome@gmail.com>
2017-05-03 19:40:49 +0000

    Updates SCRIPT.js
    Auto commit by GitBook Editor

--- SCRIPT.js
+++ SCRIPT.js
@@ -18,7 +18,7 @@ function buttonImage() {
    $("span:not([class])").replaceWith(function() {
       var username = $.trim($(this).text());
       return (
-         ''
+         '<img src="https://upload.wikimedia.org/wikipedia/en/5/54/USS_Enterprise_%28NCC-1701-A%29.jpg" width=20 height=20 align="middle">'
       );
    });
 }
