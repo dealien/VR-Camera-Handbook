9baaa227f2ada73465a1a9d13052c7db2e9f208c
dealien <dragonofawsome@gmail.com>
2017-05-03 19:37:44 +0000

    Updates SCRIPT.js
    Auto commit by GitBook Editor

--- SCRIPT.js
+++ SCRIPT.js
@@ -1,22 +1,24 @@
 document.body.onload = start;
 
 function start() {
-  testjQuery();
-  linkNames();
+   testjQuery();
+   buttonImage();
 }
 
 function testjQuery() {
-  if (!window.jQuery) {
-    console.log("jQuery is not loaded");
-  } else {
-    console.log("jQuery is loaded");
-  }
+   if (!window.jQuery) {
+      console.log("jQuery is not loaded");
+   } else {
+      console.log("jQuery is loaded");
+   }
 }
 
-function linkNames() {
-  console.log("Running function linkNames()");
-  $('span:not([class])').replaceWith(function() {
-    var username = $.trim($(this).text());
-    return '<a href="https://socialclub.rockstargames.com/member/' + username + '" target="_blank">' + username + '</a>';
-  });
+function buttonImage() {
+   console.log("Running function linkNames()");
+   $("span:not([class])").replaceWith(function() {
+      var username = $.trim($(this).text());
+      return (
+         ''
+      );
+   });
 }
